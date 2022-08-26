fa473d8a74dca60ea09f4a428a96c978adf050e4
Florentino Navarro <tino.navarro@hotmail.com>
2022-08-25 21:45:30 -0500

    site add

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://boilerplate.test',
   integrations: [react(), sitemap()],
   site: 'tinonavarrodiaz.github.io',
   base: 'boilerplate-2022',
